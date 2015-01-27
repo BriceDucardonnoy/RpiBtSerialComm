d9219b5d41b82d24bca8f6dabc9a02b4991cdd59
Brice DUCARDONNOY <brice@ducardonnoy.com>
2015-01-27 12:53:30 +0100

    Initial drop hello world

---
+++ RpiBTSerialComm/src/RpiBTSerialComm.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : RpiBTSerialComm.c
+ Author      : Brice DUCARDONNOY
+ Version     :
+ Copyright   : Copyright (c) Brice DUCARDONNOY
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World"); /* prints Hello World */
+	return EXIT_SUCCESS;
+}
